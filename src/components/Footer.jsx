428e4ce10d82a356519b3285b4cdfb74801961ef
valerievozza <valerievozza@gmail.com>
2022-10-23 13:37:53 -0700

    add social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,13 @@
 export default function Footer() {
   return (
     <footer>
-          <a href="">
+          <a href="https://github.com/valerievozza">
             <i class="fa-brands fa-square-github"></i>
           </a>
-          <a href="">
+          <a href="https://linkedin.com/in/valerievozza">
             <i class="fa-brands fa-linkedin"></i>
           </a>
-          <a href="">
+          <a href="https://twitter.com/valerievozza_">
             <i class="fa-brands fa-square-twitter"></i>
           </a>
     </footer>
